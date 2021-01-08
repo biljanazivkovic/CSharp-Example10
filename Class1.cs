213128af39d8e5dfe92b35aaa6730f7c161f2005
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:17:40 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ex10
+{
+    class Car
+    {
+        public string color = "red";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex10
+{
+    class Bike
+    {
+        string color = "blue";
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            Bike myBike = new Bike();
+            Console.WriteLine(myCar.color);
+            Console.WriteLine(myBike.color);
+        }
+    }
+}
